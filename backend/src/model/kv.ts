2627539b9b087f36a4e799b7847a86589e524cdd
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-01-26 05:44:20 +0800

    feat: refactor database handling by creating separate models for seminar and KV storage

---
+++ backend/src/model/kv.ts
@@ -0,0 +1,15 @@
+export const KVModel = (d1: D1Database) => ({
+  async get(key: string) {
+    const stmt = d1.prepare("SELECT value FROM cf_KV WHERE key = ?").bind(key);
+    const { results } = await stmt.run<{ value: string }>();
+    return results[0].value;
+  },
+  async set(key: string, value: string) {
+    await d1
+      .prepare(
+        `INSERT INTO cf_KV (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value=excluded.value`
+      )
+      .bind(key, value)
+      .run();
+  },
+});

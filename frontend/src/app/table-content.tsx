d8470cb0855818a1137e48c8c80dd00bf4803d1d
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-01-28 10:18:29 +0800

    feat: implement infinite scrolling for seminar list in TableContent component

--- frontend/src/app/table-content.tsx
+++ frontend/src/app/table-content.tsx
@@ -6,13 +6,27 @@ import { api } from "@/trpc/react";
 import { columns } from "./columns";
 
 export default function TableContent() {
-  const { data } = api.seminar.getList.useQuery();
+  const { data } = api.seminar.getList.useInfiniteQuery(
+    {
+      page: 1,
+      limit: 10,
+    },
+    {
+      getNextPageParam: (lastPage) =>
+        lastPage.meta.page < lastPage.meta.pageCount
+          ? lastPage.meta.page + 1
+          : undefined,
+    },
+  );
 
   if (!data) return null;
 
   return (
     <main className="px-4 py-8">
-      <DataTable data={data.seminars} columns={columns} />
+      <DataTable
+        data={data.pages.flatMap((page) => page.data)}
+        columns={columns}
+      />
     </main>
   );
 }
